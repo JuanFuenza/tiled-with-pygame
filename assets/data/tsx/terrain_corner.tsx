49dde927f79855194d10d19fecbaf004f2927b89
Juan Fuenzalida <juan.fuenza97@gmail.com>
2022-09-13 14:08:26 -0300

    feat: assets

---
+++ assets/data/tsx/terrain_corner.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="terrain_corner" tilewidth="128" tileheight="128" tilecount="64" columns="8">
+ <image source="../../Tilesheet/terrain_corner.png" width="1024" height="1024"/>
+</tileset>
---
+++ assets/data/tsx/tiles.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="towerDefense_tilesheet@2" tilewidth="128" tileheight="128" tilecount="299" columns="23">
+ <image source="../../Tilesheet/towerDefense_tilesheet@2.png" width="2944" height="1664"/>
+ <tile id="130" probability="0.1"/>
+ <tile id="131" probability="2"/>
+ <tile id="135" probability="0.5"/>
+ <tile id="136" probability="0.5"/>
+ <tile id="137" probability="0.5"/>
+</tileset>
